791ee37381226db0f9666d444630e40aa398615a
alvarofgomes <alvarogomes098gmail.com>
2026-02-26 18:06:19 -0300

    bloco de codigos

---
+++ inicio/bloco.js
@@ -0,0 +1,25 @@
+//ex de bloco de codigos só para saber como funciona exemplos basicos apenas para dempstração
+
+{
+    console.log("Passo #01");
+    console.log("Passo #02");
+    console.log("Passo #03");
+}
+//bloco de codigo 2
+{
+    console.log("Passo #04");
+    console.log("Passo #05");
+    console.log("Passo #06");
+}
+//bloco de codigos vazios
+{
+    {
+        {
+            {
+                ;
+                ;
+                ;
+            }
+        }
+    }
+}
