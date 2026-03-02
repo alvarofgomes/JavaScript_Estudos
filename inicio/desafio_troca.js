de2f8821707334247f845548881d535059a91fb3
alvarofgomes <alvarogomes098gmail.com>
2026-03-02 19:29:00 -0300

    desafio troca

---
+++ inicio/desafio_troca.js
@@ -0,0 +1,12 @@
+let a = 7;
+let b = 94;
+console.log("Valores originais: ");
+console.log(a);
+console.log(b);
+
+let aux = a;
+a = b;
+b = aux;
+console.log("Valores Trocados: ")
+console.log(a);
+console.log(b);
